d43018773ff73e70e334241dd569f93dd2570acb
Milton Jesus <milton.lima@outlook.com>
2023-07-20 17:12:54 -0300

    Add new libs and fix code

--- fastapi-app/main.py
+++ fastapi-app/main.py
@@ -1,47 +1,60 @@
 import random
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from fastapi.responses import HTMLResponse
-from prometheus_client import Counter
-from prometheus_client import make_asgi_app
-
-c = Counter('change_color_counter_total', 'Count the color change', ['status_code'])
+from prometheus_client import make_asgi_app, Counter, Summary, Histogram, generate_latest
+from prometheus_fastapi_instrumentator import Instrumentator
 
 
 app = FastAPI()
 
-colors = [ 'red', 'green', 'blue', 'yellow', 'orange', 'pink', 'purple']
+colors = ["red", "green", "blue", "yellow", "orange", "pink", "purple"]
+counter = Counter("change_color_counter_total", "Count the color change")
+summary = Summary(
+    "change_color_request_time_seconds",
+    "Response time in seconds to change color request",
+)
+histogram = Histogram(
+    "change_color_time_seconds",
+    "Request latency in seconds to change color",
+    )
 
 
-@c.inc
-@app.get('/changeColor', response_class=HTMLResponse)
-def change_color():
+@app.get("/changeColor", response_class=HTMLResponse)
+async def change_color():
     color = random.choice(colors)
-    return f'''
-        <html>
-            <head>
-                <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
-                <style>
-                    body {{
-                        background-color: {color};
-                        display: flex;
-                        align-items: center;
-                        justify-content: center;
-                        height: 100vh;
-                        margin: 0;
-                        font-family: 'Roboto', sans-serif;
-                    }}
-                    h1 {{
-                        font-size: 48px;
-                        text-align: center;
-                        color: white;
-                    }}
-                </style>
-            </head>
-            <body>
-                <h1>Mudar é preciso com {color}!</h1>
-            </body>
-        </html>
-    '''
+    counter.inc()
+    with summary.time():
+        with histogram.time():
+            return f"""
+                <html>
+                    <head>
+                        <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+                        <style>
+                            body {{
+                                background-color: {color};
+                                display: flex;
+                                align-items: center;
+                                justify-content: center;
+                                height: 100vh;
+                                margin: 0;
+                                font-family: 'Roboto', sans-serif;
+                            }}
+                            h1 {{
+                                font-size: 48px;
+                                text-align: center;
+                                color: white;
+                            }}
+                        </style>
+                    </head>
+                    <body>
+                        <h1>Mudar é preciso com {color}!</h1>
+                    </body>
+                </html>
+            """
 
-metrics_app = make_asgi_app()
-app.mount('/metrics', metrics_app)
+@app.get("/metrics")
+async def get_metrics():
+    return generate_latest()
+
+if __name__ == '__main__':
+    instrumentator.instrument(app).expose(app, port=8000)
